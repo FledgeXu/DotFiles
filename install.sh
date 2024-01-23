f69713f8a4df1214186f2ee9dfde4cbc40e6016b
Fledge Shiu <xzk0701@gmail.com>
2024-01-23 10:38:46 -0500

    Update.

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 brew install bat nvim gpg go fzf rg exa zoxide trash node tig htop gh llvm nvm
 # Application
-brew install appcleaner raycast telegram iterm2 qq pinentry-mac iina
+brew install appcleaner raycast telegram iterm2 qq pinentry-mac iina input-source-pro hiddenbar
