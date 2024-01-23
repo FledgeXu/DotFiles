9d2aa6e4be3091bbd1774bc6e066489253dadeea
Fledge Shiu <xzk0701@gmail.com>
2024-01-23 17:16:41 -0500

    Update.

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 brew install bat nvim gpg go fzf rg exa zoxide trash node tig htop gh llvm nvm dua-cli
 # Application
-brew install --cask appcleaner raycast iterm2 qq pinentry-mac iina input-source-pro hiddenbar textmate visual-studio-code emacs
+brew install --cask appcleaner raycast iterm2 qq pinentry-mac iina input-source-pro hiddenbar textmate visual-studio-code emacs typora
